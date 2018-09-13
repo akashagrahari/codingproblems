c386d6757888261ff0b8bf893e63f6ec3e92ac49
Akash <akash.agrahari@gmail.com>
2018-09-13 14:25:41 +0530

    bubble sort

---
+++ sorting/bubblesort.rb
@@ -0,0 +1,23 @@
+# ascending sort
+puts "Enter size of set"
+n = gets.to_i
+puts "enter numbers"
+arr = Array.new(n)
+
+for i in (0..n-1)
+	arr[i] = gets.to_i
+end
+puts "\n"
+
+
+for i in (0..n-2)
+	for j in (0..n-2-i)
+		if arr[j]>arr[j+1]
+			temp = arr[j]
+			arr[j] = arr[j+1]
+			arr[j+1] = temp
+		end
+	end
+end
+
+puts arr
