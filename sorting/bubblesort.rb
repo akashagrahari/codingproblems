bd43e7642aeeb941dc515bf0c78bcc595245667c
Akash <akash.agrahari@gmail.com>
2018-09-13 14:29:06 +0530

    More efficient bubble sort

--- sorting/bubblesort.rb
+++ sorting/bubblesort.rb
@@ -11,12 +11,17 @@ puts "\n"
 
 
 for i in (0..n-2)
+	isSorted = true
 	for j in (0..n-2-i)
 		if arr[j]>arr[j+1]
 			temp = arr[j]
 			arr[j] = arr[j+1]
 			arr[j+1] = temp
+			isSorted = false
 		end
+	end
+	if isSorted
+		break
 	end
 end
 
