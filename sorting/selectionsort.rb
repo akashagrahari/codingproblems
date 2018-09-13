dc06c9445bbcc4a163094939d4595cac04663952
Akash <akash.agrahari@gmail.com>
2018-09-13 14:14:00 +0530

    selection sort

---
+++ sorting/selectionsort.rb
@@ -0,0 +1,25 @@
+# ascending sort
+puts "Enter size of set"
+n = gets.to_i
+puts "enter numbers"
+arr = Array.new(n)
+
+for i in (0..n-1)
+	arr[i] = gets.to_i
+end
+puts "\n"
+
+
+for i in (0..n-1)
+	min = i
+	for j in (i+1..n-1)
+		 if arr[j]<arr[min]
+		 	min = j
+		 end
+	end
+	temp = arr[i]
+	arr[i] = arr[min]
+	arr[min] = temp
+end
+
+puts arr
